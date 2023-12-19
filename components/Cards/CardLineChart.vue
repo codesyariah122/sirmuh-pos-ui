d5f79be6921536fd09089808f72fb7a6c84eceaf
pujiermanto@gmail.com <pujiermanto@gmail.com>
2023-12-19 17:39:39 +0700

    laba 3 bulan terakhir

--- components/Cards/CardLineChart.vue
+++ components/Cards/CardLineChart.vue
@@ -6,10 +6,10 @@
       <div class="flex flex-wrap items-center">
         <div class="relative w-full max-w-full flex-grow flex-1">
           <h6 class="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
-            Overview
+            Laba
           </h6>
           <h2 class="text-white text-xl font-semibold">
-            Sales value
+            {{ title }}
           </h2>
         </div>
       </div>
@@ -26,111 +26,161 @@
 import Chart from "chart.js";
 
 export default {
+  data() {
+    return {
+      api_url: process.env.NUXT_ENV_API_URL,
+      profits: [],
+      loading: false,
+      title: "",
+      panelCharts: [],
+    };
+  },
+  beforeMount() {
+    this.authTokenStorage();
+  },
+  methods: {
+    authTokenStorage() {
+      this.$store.dispatch("auth/storeAuthToken", "auth");
+    },
+  },
+
   mounted: function () {
     this.$nextTick(function () {
-      var config = {
-        type: "line",
-        data: {
-          labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-          ],
-          datasets: [
-            {
-              label: new Date().getFullYear(),
-              backgroundColor: "#4c51bf",
-              borderColor: "#4c51bf",
-              data: [65, 78, 66, 44, 56, 67, 75],
-              fill: false,
-            },
-            {
-              label: new Date().getFullYear() - 1,
-              fill: false,
-              backgroundColor: "#fff",
-              borderColor: "#fff",
-              data: [40, 68, 86, 74, 56, 60, 87],
-            },
-          ],
-        },
-        options: {
-          maintainAspectRatio: false,
-          responsive: true,
-          title: {
-            display: false,
-            text: "Sales Charts",
-            fontColor: "white",
-          },
-          legend: {
-            labels: {
-              fontColor: "white",
-            },
-            align: "end",
-            position: "bottom",
-          },
-          tooltips: {
-            mode: "index",
-            intersect: false,
-          },
-          hover: {
-            mode: "nearest",
-            intersect: true,
-          },
-          scales: {
-            xAxes: [
-              {
-                ticks: {
-                  fontColor: "rgba(255,255,255,.7)",
-                },
-                display: true,
-                scaleLabel: {
-                  display: false,
-                  labelString: "Month",
-                  fontColor: "white",
-                },
-                gridLines: {
-                  display: false,
-                  borderDash: [2],
-                  borderDashOffset: [2],
-                  color: "rgba(33, 37, 41, 0.3)",
-                  zeroLineColor: "rgba(0, 0, 0, 0)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-            yAxes: [
-              {
-                ticks: {
-                  fontColor: "rgba(255,255,255,.7)",
-                },
-                display: true,
-                scaleLabel: {
-                  display: false,
-                  labelString: "Value",
-                  fontColor: "white",
-                },
-                gridLines: {
-                  borderDash: [3],
-                  borderDashOffset: [3],
-                  drawBorder: false,
-                  color: "rgba(255, 255, 255, 0.15)",
-                  zeroLineColor: "rgba(33, 37, 41, 0)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-          },
+      const jmlMonth = 3;
+      const endPoint = `/laba-rugi/${jmlMonth}`;
+      const configApi = {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${this?.token?.token}`,
         },
       };
-      var ctx = document.getElementById("line-chart").getContext("2d");
-      window.myLine = new Chart(ctx, config);
+
+      this.$api.defaults.headers.common["Sirmuh-Key"] =
+        process.env.NUXT_ENV_APP_TOKEN;
+      this.$api
+        .get(endPoint, configApi)
+        .then(({ data }) => {
+          this.title = data.message;
+          const newData = data?.data.map((item) => {
+            const { year, month, total_laba } = item;
+            const monthName = this.$moment({ year, month: month - 1 }).format(
+              "MMMM"
+            );
+            return { monthName, total_laba, year };
+          });
+
+          let config = {
+            type: "bar",
+            data: {
+              labels: newData.map((item) => item.monthName),
+              datasets: [
+                {
+                  label: new Date().getFullYear(),
+                  backgroundColor: ["#4c51bf", "rgba(245, 40, 145, 0.8)"],
+                  borderColor: "#4c51bf",
+                  data: newData.map((item) => item.total_laba),
+                  fill: false,
+                },
+              ],
+            },
+            options: {
+              maintainAspectRatio: false,
+              responsive: true,
+              title: {
+                display: false,
+                text: "Sales Charts",
+                fontColor: "white",
+              },
+              legend: {
+                labels: {
+                  fontColor: "white",
+                },
+                align: "end",
+                position: "bottom",
+                generateLabels: function (chart) {
+                  const labels =
+                    Chart.defaults.global.legend.labels.generateLabels(chart);
+
+                  labels.forEach((label) => {
+                    const datasetIndex = label.datasetIndex;
+                    label.text = `${label.text} (${newData[datasetIndex].year})`;
+                  });
+
+                  return labels;
+                },
+              },
+              tooltips: {
+                mode: "index",
+                intersect: false,
+              },
+              hover: {
+                mode: "nearest",
+                intersect: true,
+              },
+              scales: {
+                xAxes: [
+                  {
+                    ticks: {
+                      fontColor: "rgba(255,255,255,.7)",
+                    },
+                    display: true,
+                    scaleLabel: {
+                      display: false,
+                      labelString: "Month",
+                      fontColor: "white",
+                    },
+                    gridLines: {
+                      display: false,
+                      borderDash: [2],
+                      borderDashOffset: [2],
+                      color: "rgba(33, 37, 41, 0.3)",
+                      zeroLineColor: "rgba(0, 0, 0, 0)",
+                      zeroLineBorderDash: [2],
+                      zeroLineBorderDashOffset: [2],
+                    },
+                  },
+                ],
+                yAxes: [
+                  {
+                    ticks: {
+                      fontColor: "rgba(255,255,255,.7)",
+                    },
+                    display: true,
+                    scaleLabel: {
+                      display: false,
+                      labelString: "Value",
+                      fontColor: "white",
+                    },
+                    gridLines: {
+                      borderDash: [3],
+                      borderDashOffset: [3],
+                      drawBorder: false,
+                      color: "rgba(255, 255, 255, 0.15)",
+                      zeroLineColor: "rgba(33, 37, 41, 0)",
+                      zeroLineBorderDash: [2],
+                      zeroLineBorderDashOffset: [2],
+                    },
+                  },
+                ],
+              },
+            },
+          };
+
+          var ctx = document.getElementById("line-chart").getContext("2d");
+          window.myLine = new Chart(ctx, config);
+        })
+        .finally(() => {
+          setTimeout(() => {
+            this.loading = false;
+          }, 1500);
+        });
     });
+  },
+
+  computed: {
+    token() {
+      return this.$store.getters["auth/getAuthToken"];
+    },
   },
 };
 </script>
