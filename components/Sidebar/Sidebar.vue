8af4ea93872b791bde20b4a772cf294097403cee
pujiermanto@gmail.com <pujiermanto@gmail.com>
2024-01-04 01:22:38 +0700

    update ae

--- components/Sidebar/Sidebar.vue
+++ components/Sidebar/Sidebar.vue
@@ -22,15 +22,33 @@
         <i class="fas fa-bars"></i>
       </button>
       <!-- Brand -->
-      <router-link
-        class="md:block hidden text-left md:pb-2 mb-4 mr-0 lg:inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
-        :to="`/dashboard/${roles}`"
-      >
-        <img
-          :src="require('~/assets/img/new-logo.png')"
-          style="max-width: 200px"
-        />
-      </router-link>
+      <div class="flex justify-start">
+        <div>
+          <router-link
+            class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+            :to="`/dashboard/${roles}`"
+          >
+            <img
+              :src="`${storage_url}/tokos/${companies.logo}`"
+              style="max-width: 150px"
+            />
+          </router-link>
+        </div>
+      </div>
+
+      <div class="flex justify-start">
+        <div>
+          <h4 class="font-bold">
+            {{ companies.name }}
+          </h4>
+          <blockquote class="text-left font-semibold text-xs capitalize">
+            {{ companies.about }}
+          </blockquote>
+          <address class="text-sm mt-2">
+            {{ companies.address }}
+          </address>
+        </div>
+      </div>
 
       <!-- User -->
       <ul class="md:hidden items-center flex flex-wrap list-none">
@@ -51,16 +69,57 @@
           class="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200"
         >
           <div class="flex flex-wrap">
-            <div class="w-6/12 lg:block hidden">
-              <router-link
-                class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
-                :to="`/dashboard/${roles}`"
-              >
-                <img
-                  :src="require('~/assets/img/new-logo.png')"
-                  style="max-width: 150px"
-                />
-              </router-link>
+            <div v-if="loadingData" class="w-6/12 lg:block hidden">
+              <div class="text-center">
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span class="sr-only">Loading...</span>
+                </div>
+              </div>
+            </div>
+            <div v-else class="w-6/12 lg:block hidden">
+              <div class="flex justify-start">
+                <div>
+                  <router-link
+                    class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+                    :to="`/dashboard/${roles}`"
+                  >
+                    <img
+                      :src="`${storage_url}/tokos/${companies.logo}`"
+                      style="max-width: 150px"
+                    />
+                  </router-link>
+                </div>
+              </div>
+
+              <div class="flex justify-center">
+                <div>
+                  <h4 class="font-bold">
+                    {{ companies.name }}
+                  </h4>
+                  <blockquote class="text-left">
+                    {{ companies.about }}
+                  </blockquote>
+                  <address>
+                    {{ companies.address }}
+                  </address>
+                </div>
+              </div>
             </div>
             <div class="w-6/12 flex justify-end">
               <button
@@ -204,9 +263,11 @@ export default {
       name: "",
       path: this.$route.path,
       image_url: process.env.NUXT_ENV_ASSET_PUBLIC_URL,
+      storage_url: process.env.NUXT_ENV_STORAGE_URL,
       photo: "",
       messageNotif: "",
       menuSubMenuNotifs: [],
+      companies: [],
     };
   },
 
@@ -225,6 +286,7 @@ export default {
 
   mounted() {
     this.checkIsLogin();
+    this.getDataPerusahaan();
   },
 
   methods: {
@@ -242,6 +304,37 @@ export default {
 
     authTokenStorage() {
       this.$store.dispatch("auth/storeAuthToken", "auth");
+    },
+
+    getDataPerusahaan() {
+      this.loadingData = true;
+      const endPoint = `/data-perusahaan`;
+      const config = {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${this?.token?.token}`,
+          "Sirmuh-Key": process.env.NUXT_ENV_APP_TOKEN,
+        },
+      };
+
+      this.$api.defaults.headers.common["Sirmuh-Key"] =
+        process.env.NUXT_ENV_APP_TOKEN;
+      this.$api
+        .get(endPoint, config)
+        .then(({ data }) => {
+          this.companies = data?.data[0];
+          console.log(this.companies);
+        })
+        .finally(() => {
+          setTimeout(() => {
+            this.loadingData = false;
+          }, 1500);
+        })
+        .catch((err) => {
+          if (err.error) {
+            this.sesiLogout(this.roles ? this.roles : "");
+          }
+        });
     },
 
     checkIsLogin() {
