67d3d5cb585087677a6b6f6d7d329b82f8e26841
pujiermanto@gmail.com <pujiermanto@gmail.com>
2024-01-25 21:20:19 +0700

    git stash

---
+++ components/LaporanPembelianPeriode/TrashCell.vue
@@ -0,0 +1,161 @@
+<template>
+  <tbody>
+    <tr v-for="column in columns" :key="column.id">
+      <th
+        class="border-t-0 px-6 border-l-0 border-r-0 text-xs w-12 p-4 text-left"
+        style="width: 50px"
+      >
+        {{ column.kode }}
+      </th>
+
+      <td
+        class="border-t-0 px-8 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ column.nama }}
+      </td>
+
+      <td
+        v-if="column.photo !== null"
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        <img :src="`${image_url}/${column.photo}`" class="w-[600px]" />
+      </td>
+
+      <td
+        v-else
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        <img
+          :src="require('~/assets/img/default.jpg')"
+          alt="default image product if no photo product"
+        />
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ $moment(column.tgl_terakhir).locale("id").format("dddd, D MMMM") }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ column.kategori }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ column.satuanbeli }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ column.satuan }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ column.isi }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ column.stok }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ $format(column.hpp) }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ $format(column.harga_toko) }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ column.diskon }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{ column.supplier }}
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        <img
+          :src="`${image_url}/qrcodes/${column.barcode}.png`"
+          class="w-[600px]"
+        />
+      </td>
+
+      <td
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+      >
+        {{
+          column.expired !== null
+            ? $moment(column.expired).locale("id").format("dddd, D MMMM")
+            : "-"
+        }}
+      </td>
+
+      <td
+        v-if="column.token !== token.token && column.name !== 'VICKY ANDRIANI'"
+        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left"
+      >
+        <dropdowns-table-dropdown
+          @deleted-data="deletedData"
+          @restored-data="restoredData"
+          :id="column.id"
+          :types="types"
+          cellType="trash"
+        />
+      </td>
+    </tr>
+  </tbody>
+</template>
+
+<script>
+export default {
+  props: {
+    columns: {
+      type: Array,
+      default: function () {
+        return {}; // or any other appropriate default value
+      },
+    },
+    types: {
+      type: String,
+    },
+  },
+
+  data() {
+    return {
+      image_url: process.env.NUXT_ENV_STORAGE_URL,
+    };
+  },
+
+  mounted() {},
+
+  methods: {
+    deletedData(id) {
+      this.$emit("deleted-data", id);
+    },
+
+    restoredData(id) {
+      this.$emit("restored-data", id);
+    },
+  },
+};
+</script>
