94e8108b1cb28921a86c42f4100d4da9ad6c319c
pujiermanto@gmail.com <pujiermanto@gmail.com>
2024-01-31 02:51:51 +0700

    update lagi

--- pages/dashboard/transaksi/beli/pembelian-langsung/index.vue
+++ pages/dashboard/transaksi/beli/pembelian-langsung/index.vue
@@ -209,11 +209,8 @@ export default {
   watch: {
     notifs() {
       if (this.$_.size(this.$nuxt.notifs) > 0) {
-        console.log(this.$nuxt.notifs[0].routes);
         if (this.$nuxt.notifs[0].routes === "pembelian-langsung") {
-          this.getPembelianLangsung(
-            this.paging.current ? this.paging.current : 1
-          );
+          this.getPembelianLangsung(this.paging.current, {}, false);
         }
       }
     },
