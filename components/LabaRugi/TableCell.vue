4b774accece1d6a4e0fd37a5186f4630f97a2395
pujiermanto@gmail.com <pujiermanto@gmail.com>
2024-01-31 09:35:56 +0700

    update lagi

--- components/LabaRugi/TableCell.vue
+++ components/LabaRugi/TableCell.vue
@@ -30,7 +30,7 @@
       <td
         class="border-t-0 px-8 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
       >
-        {{ $roundup(column.diskon) }}
+        {{ $roundup(column.diskon) }}%
       </td>
       <td
         class="border-t-0 px-8 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
