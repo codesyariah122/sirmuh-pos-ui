6b74857a5aac3f34595c3529090859ae65c89d37
root <root@digitalkreatifsolusindo.cc>
2024-02-21 10:02:29 +0700

    update lagi

--- components/Cashes/FilterKas.vue
+++ components/Cashes/FilterKas.vue
@@ -54,8 +54,8 @@
             </div>
 
             <div v-bind:class="{ hidden: openTab !== 3, block: openTab === 3 }">
-              <div class="flex justify-center">
-                <div class="flex-none w-full">
+              <div class="flex justify-between space-x-4">
+                <div class="shrink w-[85%]">
                   <Select2
                     v-model="selectedKas"
                     :settings="{ allowClear: true }"
@@ -64,6 +64,14 @@
                     @select="changeKas($event)"
                     placeholder="Pilih Kode Kas"
                   />
+                </div>
+                <div>
+                  <button
+                    @click="clearSelectedCategory"
+                    class="text-white bg-red-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                  >
+                    <i class="fa-solid fa-filter-circle-xmark"></i>
+                  </button>
                 </div>
               </div>
             </div>
@@ -114,6 +122,17 @@ export default {
   methods: {
     toggleTabs: function (tabNumber) {
       this.openTab = tabNumber;
+    },
+
+    clearSelectedCategory() {
+      this.selectedCategory = null;
+      this.clearKey += 1;
+      this.$emit("filter-data", {
+        nama: "",
+        kode: null,
+        start_date: "",
+        end_date: "",
+      });
     },
 
     changeKas(newValues) {
