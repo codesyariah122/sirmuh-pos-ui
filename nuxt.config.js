2ef10eecad0b45e712bf4d4159e4a8ebcbbbecac
root <root@digitalkreatifsolusindo.cc>
2024-03-11 23:17:33 +0700

    udpate fix error

--- nuxt.config.js
+++ nuxt.config.js
@@ -205,6 +205,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: process.env.NUXT_ENV_API_URL,
+    credential: true
   },
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
