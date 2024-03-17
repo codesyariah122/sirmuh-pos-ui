573b4616d26cd384fb3ce6bfeccb6f8aae360b35
root <root@digitalkreatifsolusindo.cc>
2024-03-17 11:30:40 +0700

    update aje

--- components/TerimaPiutang/Pelanggan/DetailTableCell.vue
+++ components/TerimaPiutang/Pelanggan/DetailTableCell.vue
@@ -67,7 +67,7 @@
     </td>
     <td class="px-6 py-4" v-else>
       <span
-        class="bg-green-100 text-green-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400"
+        :class="`${item.visa === 'HUTANG' ? 'bg-red-100 text-red-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400' : 'bg-green-100 text-green-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400'}`"
         >{{ item.visa }}</span
       >
     </td>
