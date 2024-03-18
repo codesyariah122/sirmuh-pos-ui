ec3e81219fb16041aa124eac2625c8317fd1abcd
root <root@digitalkreatifsolusindo.cc>
2024-03-18 08:12:25 +0700

    update aja

--- components/Sidebar/Sidebar.vue
+++ components/Sidebar/Sidebar.vue
@@ -15,10 +15,10 @@
       </button>
       <!-- Brand -->
       <router-link
-        class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+        class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0 text-emerald-600 hover:bg-blueGray-800 hover:rounded-md hover:text-emerald-600"
         to="/"
       >
-        {{ companies.name }}
+        <i class="fa-solid fa-door-open fa-2xl"></i> Halaman Awal
       </router-link>
       <div class="flex justify-start">
         <div>
