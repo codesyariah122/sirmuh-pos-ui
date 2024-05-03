5222d5c869bc63bed8836a07c5eea0dd554b7417
codesyariah122 <pujiermanto@gmail.com>
2024-05-04 01:14:49 +0700

    update puji aje

--- components/Buys/PurchaseOrder/Form/AcceptPurchaseOrderForm.vue
+++ components/Buys/PurchaseOrder/Form/AcceptPurchaseOrderForm.vue
@@ -700,6 +700,36 @@
       </div>
     </li>
 
+    <li v-if="parseFloat(detail?.saldo_hutang) !== 0" class="py-6">
+      <div class="grid grid-cols-3 gap-0">
+        <div>
+          <label class="font-bold">Saldo Hutang</label>
+        </div>
+        <div>
+          <input
+          disabled
+          type="text"
+          value="0"
+          class="h-8 text-black"
+          v-model="detail.saldo_hutang"
+          />
+        </div>
+      </div>
+      <div class="flex justify-start mb-4 py-2">
+        <div>
+          <div class="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>
+              <span class="font-medium">Jumlah Saldo Hutang!</span> masih ada saldo hutang sebesar : {{detail.saldo_hutang}}.
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+
     <!-- Diskon & PPN Disabled dulu -->
             <!-- <li class="w-full py-2">
               <div class="grid grid-cols-3 gap-0">
--- pages/dashboard/transaksi/beli/purchase-order/terima-po/_id.vue
+++ pages/dashboard/transaksi/beli/purchase-order/terima-po/_id.vue
@@ -4,25 +4,25 @@
       <molecules-row-loading :loading="loadingStart" :options="options" />
     </div>
     <div v-else
-      :class="`w-full ${
-        routeName === 'edit' ? 'lg:w-12/12' : 'lg:w-12/12'
-      } px-4`"
+    :class="`w-full ${
+      routeName === 'edit' ? 'lg:w-12/12' : 'lg:w-12/12'
+    } px-4`"
     >
-      <cards-card-settings
-        color="dark"
-        pageType="terimaPurchaseOrder"
-        link="transaksi"
-        :title="`Terima P.O : ${faktur}`"
-        methodType="accept"
-        :type="type"
-        pageData="/transaksi/beli/purchase-order"
-        :detail="detail"
-        :items="items"
-        :orders="orders"
-        @rebuild-data="getDetailPembelian"
-      />
-    </div>
+    <cards-card-settings
+    color="dark"
+    pageType="terimaPurchaseOrder"
+    link="transaksi"
+    :title="`Terima P.O : ${faktur}`"
+    methodType="accept"
+    :type="type"
+    pageData="/transaksi/beli/purchase-order"
+    :detail="detail"
+    :items="items"
+    :orders="orders"
+    @rebuild-data="getDetailPembelian"
+    />
   </div>
+</div>
 </template>
 
 <script>
@@ -31,62 +31,63 @@
  * @returns {string}
  * @author Puji Ermanto <puji.ermanto@gmail.com>
  */
-export default {
-  name: "terima-purchase-order",
-  layout: "admin",
+  export default {
+    name: "terima-purchase-order",
+    layout: "admin",
 
-  data() {
-    return {
-      id: this.$route.params.id,
-      detail: {},
-      items: [],
-      orders: [],
-      routeName: this.$route.name.split("-").pop(),
-      loadingStart: true,
-      options: 'terima-purchase-order',
-      successNew: null,
-      messageNew: "",
-      detail: {},
-      type: this.$route.query["type"],
-      faktur: this.$route.query["faktur"]
-    };
-  },
-
-  beforeMount() {
-    this.authTokenStorage();
-    this.storedFormData();
-  },
-
-  created() {
-    this.$nuxt.checkNewData();
-  },
-
-  mounted() {
-    this.getDetailPembelian(true);
-  },
-
-  methods: {
-    getDetailPembelian(loading) {
-      this.loadingStart = loading
-      if(loading) {
-        this.$nuxt.globalLoadingMessage =
-        "Proses menyiapkan data purchase order ...";
-      }
-
-      const endPoint = `/data-purchase-order/${this.id}`;
-      const config = {
-        headers: {
-          Accept: "application/json",
-          Authorization: `Bearer ${this.token.token}`,
-        },
+    data() {
+      return {
+        id: this.$route.params.id,
+        detail: {},
+        items: [],
+        orders: [],
+        routeName: this.$route.name.split("-").pop(),
+        loadingStart: true,
+        options: 'terima-purchase-order',
+        successNew: null,
+        messageNew: "",
+        detail: {},
+        type: this.$route.query["type"],
+        faktur: this.$route.query["faktur"]
       };
+    },
 
-      this.$api
+    beforeMount() {
+      this.authTokenStorage();
+      this.storedFormData();
+    },
+
+    created() {
+      this.$nuxt.checkNewData();
+    },
+
+    mounted() {
+      this.getDetailPembelian(true);
+    },
+
+    methods: {
+      getDetailPembelian(loading) {
+        this.loadingStart = loading
+        if(loading) {
+          this.$nuxt.globalLoadingMessage =
+          "Proses menyiapkan data purchase order ...";
+        }
+
+        const endPoint = `/data-purchase-order/${this.id}`;
+        const config = {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${this.token.token}`,
+          },
+        };
+
+        this.$api
         .get(endPoint, config)
         .then((data) => {
           this.detail = data.data.data;
           this.items = data.data.items;
           this.orders = data.data.purchase_orders;
+          console.log(this.detail)
         })
         .finally(() => {
           setTimeout(() => {
@@ -96,31 +97,31 @@ export default {
         .catch((err) => {
           console.log(err);
         });
+      },
+      storedFormData() {
+        this.$store.dispatch("success/storedFormData", "success-form");
+      },
     },
-    storedFormData() {
-      this.$store.dispatch("success/storedFormData", "success-form");
-    },
-  },
 
-  computed: {
-    formData() {
-      return this.$store.getters["success/formData"];
+    computed: {
+      formData() {
+        return this.$store.getters["success/formData"];
+      },
     },
-  },
 
-  watch: {
-    notifs() {
-      if (this.$nuxt.notifs && this.$_.size(this.$nuxt.notifs) > 0) {
-        if (this.$nuxt.notifs.find(notif => notif.routes === 'purchase-order') || this.$nuxt.notifs.find(notif => notif.routes === 'data-barang')) {
-          this.storedFormData();
+    watch: {
+      notifs() {
+        if (this.$nuxt.notifs && this.$_.size(this.$nuxt.notifs) > 0) {
+          if (this.$nuxt.notifs.find(notif => notif.routes === 'purchase-order') || this.$nuxt.notifs.find(notif => notif.routes === 'data-barang')) {
+            this.storedFormData();
           // Memastikan bahwa notif berupa objek atau tidak kosong sebelum memberikan nilai ke loading
-          this.getDetailPembelian(false);
+            this.getDetailPembelian(false);
+          }
         }
-      }
+      },
+      token() {
+        return this.$store.getters["auth/getAuthToken"];
+      },
     },
-    token() {
-      return this.$store.getters["auth/getAuthToken"];
-    },
-  },
-};
+  };
 </script>
